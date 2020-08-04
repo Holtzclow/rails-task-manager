9cdaf3f45c408d9712e7011b4bf8953980803c5c
veronica puspita <caslavia@gmail.com>
2020-08-04 14:34:34 +0900

    adding CRUD on task

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_233259) do
+ActiveRecord::Schema.define(version: 2020_08_04_021453) do
 
   create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
